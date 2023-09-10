31b2394da87dfa92314c96fe57682b4e11fdfe9a
sergey s <som-ana@yandex.ru>
2023-09-10 11:06:26 +0300

    третье задание

--- bjs-2-homeworks/3.arrays/task.js
+++ bjs-2-homeworks/3.arrays/task.js
@@ -1,13 +1,15 @@
+//       1-я задача
 function compareArrays(arr1, arr2) {
-    return (JSON.stringify(arr1) === JSON.stringify(arr2));
-  }
+	return (JSON.stringify(arr1) === JSON.stringify(arr2));
+}
 
-  function getUsersNamesInAgeRange(users, gender) {
-    return users.filter(user => user.gender === gender).map(result => result.age).reduce((summ, userAge, index, array) => {
-      summ += userAge;
-      if (index === array.length - 1) {
-        return summ / array.length;
-      }
-      return summ;
-    }, 0);
-  }
+//        2-я задача
+function getUsersNamesInAgeRange(users, gender) {
+	return users.filter(user => user.gender === gender).map(result => result.age).reduce((summ, userAge, index, array) => {
+		summ += userAge;
+		if (index === array.length - 1) {
+			return summ / array.length;
+		}
+		return summ;
+	}, 0);
+}
