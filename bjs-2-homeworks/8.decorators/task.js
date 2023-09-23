7f16a336e2ec57a4954e31eec9374548876d5e3c
sergey s <som-ana@yandex.ru>
2023-09-24 00:38:00 +0300

    восьмое задание

--- bjs-2-homeworks/8.decorators/task.js
+++ bjs-2-homeworks/8.decorators/task.js
@@ -1,9 +1,51 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
-}
+    let cache = [];
+
+    return function (...args) {
+      const hash = md5(args);
+      let objectInCache = cache.find(
+        (objectInCache) => objectInCache.hash === hash
+      );
+
+      if (objectInCache) {
+        console.log("Из кэша: " + objectInCache.result);
+        return "Из кэша: " + objectInCache.result;
+      }
+
+      let result = func(...args);
+      cache.push({ hash, result });
+
+      if (cache.length > 5) {
+        cache.shift();
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+    };
+  }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
-}
+    let timeoutId = null;
+    function wrapper(...args) {
+      if (timeoutId === null) {
+        func(...args);
+        wrapper.count++;
+      }
+
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+
+      timeoutId = setTimeout(() => {
+        timeoutId = true;
+        func(...args);
+        wrapper.count++;
+      }, delay);
+      wrapper.allCount++;
+    }
+
+    wrapper.count = 0;
+    wrapper.allCount = 0;
+    return wrapper;
+  }
